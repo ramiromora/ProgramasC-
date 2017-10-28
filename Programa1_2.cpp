2412cb714a18dbd28dc44cda44d4218a22d35a4e
Ramiro Mora <rvmora@gmail.com>
2017-10-28 10:12:57 -0400

    Create Programa1_2.cpp

---
+++ Programa1_2.cpp
@@ -0,0 +1,14 @@
+//TiposDeDatos.cpp
+#include <iostream>
+using namespace std;
+
+int main(){
+ cout << "Dimension de char : " << sizeof(char) << endl;
+ cout << "Dimension de int : " << sizeof(int) << endl;
+ cout << "Dimension de short int : " << sizeof(short int) << endl;
+ cout << "Dimension de long int : " << sizeof(long int) << endl;
+ cout << "Dimension de float : " << sizeof(float) << endl;
+ cout << "Dimension de double : " << sizeof(double) << endl;
+ cout << "Dimension de wchar_t : " << sizeof(wchar_t) << endl;
+ return 0;
+}
